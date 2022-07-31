7634a7a7ecb616e1a86edd1ad7efebb273456bcf
wisdom08 <wisdom08@kakao.com>
2022-07-31 14:08:25 +0900

    #16: 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 멩세를 뽑는다.

---
+++ src/main/java/com/wisdom/projectboard/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.wisdom.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/article")
+@Controller
+public class ArticleController {
+
+}
